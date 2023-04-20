932b66c7e1aefba3fb693fe896ebef9aa5789a1c
Eric Hines <hines.eric.m@gmail.com>
2023-04-19 22:05:13 -0500

    completed create addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,15 @@
 console.log('***** Music Collection *****')
 let collection = [];
+
+function addToCollection(titleOfAlbum, artistName, yearPublishedDate) {
+    library = {
+        title: titleOfAlbum,
+        artist: artistName,
+        yearPublished: yearPublishedDate,
+    };
+    collection.push(library);
+    return collection;
+}
+
+console.log(addToCollection('Blizzard of Ozz', 'Ozzy Osbourne', 1980));
+console.log(addToCollection('Love at First Sting', 'Klaus Meine', 1984));
