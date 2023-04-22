0779186756051dab0969a931ceeadb1d2dda78b8
Eric Hines <hines.eric.m@gmail.com>
2023-04-21 21:35:20 -0500

    completed completed addToCollection array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,3 +13,7 @@ function addToCollection(titleOfAlbum, artistName, yearPublishedDate) {
 
 console.log(addToCollection('Blizzard of Ozz', 'Ozzy Osbourne', 1980));
 console.log(addToCollection('Love at First Sting', 'Klaus Meine', 1984));
+console.log(addToCollection('Raise!', 'Maurice White', 1981));
+console.log(addToCollection('Paranoid', 'Ozzy Osbourne', 1970));
+console.log(addToCollection('Trash', 'Alice Cooper', 1989));
+console.log(addToCollection('Diana','Diana Ross', 1980));
